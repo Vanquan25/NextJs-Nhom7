556dcefb5a9eb6978fb4793e13584b0cb12a1b46
vanquan25 <minhquan22012@gmail.com>
2022-07-27 10:54:07 +0700

    fix login

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,22 +4,40 @@ type Props = {}
 
 const contact = (props: Props) => {
   return (
-    <div className="max-w-lg mx-auto">
-  <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm mb-5">
-    <a href="#">
-      <img className="rounded-t-lg" src="https://picsum.photos/300" />
-    </a>
-    <div className="p-5">
-      <a href="#">
-        <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">Noteworthy technology acquisitions 2021</h5>
-      </a>
-      <p className="font-normal text-gray-700 mb-3">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-      <a className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center" href="#">
-        Read more
-      </a>
+    <div className="py-16 bg-gradient-to-br from-green-50 to-cyan-100">  
+  <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+    <div className="mb-12 space-y-2 text-center">
+      <span className="block w-max mx-auto px-3 py-1.5 border border-green-200 rounded-full bg-green-100 text-green-600">Blog</span>
+      <h2 className="text-2xl text-cyan-900 font-bold md:text-4xl">Sharing is Caring</h2>
+      <p className="lg:w-6/12 lg:mx-auto">Quam hic dolore cumque voluptate rerum beatae et quae, tempore sunt, debitis dolorum officia aliquid explicabo? Excepturi, voluptate?</p>
+    </div>
+    <div className="grid gap-12 lg:grid-cols-2">
+      <div className="p-1 rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
+        <img src="https://tailus.io/sources/blocks/twocards/preview/images/woman.jpg" alt="art cover" loading="lazy" width={1000} height={667} className="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl" />
+        <div className="sm:w-7/12 pl-0 p-5">
+          <div className="space-y-2">
+            <div className="space-y-4">
+              <h4 className="text-2xl font-semibold text-cyan-900">Provident de illo eveniet commodi fuga fugiat laboriosam expedita.</h4>
+              <p className="text-gray-600">Laborum saepe laudantium in, voluptates ex placeat quo harum aliquam totam, doloribus eum impedit atque! Temporibus...</p>
+            </div>
+            <a href="www.tailus.io" className="block w-max text-cyan-600">Read more</a>
+          </div>
+        </div>
+      </div>
+      <div className="p-1 rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
+        <img src="https://tailus.io/sources/blocks/twocards/preview/images/man.jpg" alt="art cover" loading="lazy" width={1000} height={667} className="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl" />
+        <div className="sm:w-7/12 pl-0 p-5">
+          <div className="space-y-2">
+            <div className="space-y-4">
+              <h4 className="text-2xl font-semibold text-cyan-900">Provident de illo eveniet commodi fuga fugiat laboriosam expedita.</h4>
+              <p className="text-gray-600">Laborum saepe laudantium in, voluptates ex placeat quo harum aliquam totam, doloribus eum impedit atque! Temporibus...</p>
+            </div>
+            <a href="www.tailus.io" className="block w-max text-cyan-600">Read more</a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-  <p>This card component is part of a larger, open-source library of Tailwind CSS components. Learn more by going to the official <a className="text-blue-600 hover:underline" href="#" target="_blank">Flowbite Documentation</a>.</p>
 </div>
 
 
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
@@ -37,6 +38,9 @@ const signin = (props: Props) => {
             </div>
             <div>
               <label className="inline-flex items-center cursor-pointer"><input id="customCheckLogin" type="checkbox" className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150" /><span className="ml-2 text-sm font-semibold text-blueGray-600">Remember me</span></label>
+            </div>
+            <div className="text-center mt-6">
+                <Link href="/signup">Sign Up</Link>
             </div>
             <div className="text-center mt-6">
               <button className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150" type="button"> Sign In </button>
